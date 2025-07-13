3aa8ecca43f25ef2c4c9420a2f18b3129391e2d5
adhamahmed97100 <adhamahmed97100@gmail.com>
2025-07-13 15:43:59 +0300

    Fix: allow environments folder and add prod environment file

---
+++ projects/user/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  Ip: 'https://9223-102-46-178-55.ngrok-free.app',
+  firebase: {
+    apiKey: 'AIzaSyAtxBlXbktsYinFT-ubAAHdH8pX0xotzHU',
+    authDomain: 'chat-3afa0.firebaseapp.com',
+    projectId: 'chat-3afa0',
+    storageBucket: 'chat-3afa0.firebasestorage.app',
+    messagingSenderId: '213036366957',
+    appId: '1:213036366957:web:7163e1647998532cd11364',
+    measurementId: 'G-GM49HM2W8V',
+    vapidKey:
+      'BD5RLll_hzgsuIMp1ksRmFKQwsX1JlEeMCe8B0en-zd5kEyIV-i8IKEg-qZt563Cg6sOwPb-s-nWLtrxvRKgK7M',
+  },
+};
